00f5915a8125d5fb6f1dea3de9e69f895d5301c4
aQu4123 <aqu4123@gmail.com>
2025-02-03 03:40:40 +0300

    added site /news/

--- news/models.py
+++ news/models.py
@@ -19,6 +19,8 @@ class Author(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length = 64, unique = True)
 
+    def __str__(self):
+        return self.name.title()
 
 class Post(models.Model):
     author = models.ForeignKey(Author, on_delete = models.CASCADE)
@@ -39,6 +41,9 @@ class Post(models.Model):
 
     def preview(self):
         return self.text[:124] + '. . .'
+
+    def __str__(self):
+        return f'{self.title.title()}: {self.text[:20]}'
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete = models.CASCADE)
